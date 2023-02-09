50a3b6cdc6c1c9779985703d7b17bbe3ff1d02cc
sufyan <mdsufyan7@gmail.com>
2023-02-09 13:00:29 +0530

    Initial commit

---
+++ donation_options.php
@@ -0,0 +1,72 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<!-- -- Semantic-UI CSS & JS files included here -- -->
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/button.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/table.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/icon.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/dropdown.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/transition.min.css">
+<style type="text/css">
+    div.ui.dropdown{
+        min-height: 1em !important;
+    }
+</style>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/dropdown.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/transition.js"></script>
+
+<h1>Settings</h1>
+<?php
+global $wpdb;
+$user_id = get_current_user_id();
+if(isset($_POST["submit"])){
+    $data["payee_name"] = $_POST["payee_name"];
+    $data["upi_link"] = $_POST["upi_link"];
+    $data["amount"] = $_POST["amount"];
+    foreach ($data as $key => $value) {
+        update_option($key, $value);
+    }
+}
+?>
+<form method="post" enctype="multipart/form-data">
+    <table class="ui collapsing striped table">
+        <tr>
+            <td>Payee Name</td>
+            <td><input type="text" name="payee_name" >
+            </td>
+        </tr>
+        <tr>
+            <td>Upi Link</td>
+            <td><input type="text" name="upi_link" >
+            </td>
+        </tr>
+        <tr>
+            <td>Amount</td>
+            <td><input type="text" name="amount" >
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" name="submit" value="Save" class="ui blue mini button"></td>
+        </tr>
+    </table>
+</form>
+<?php
+$data["payee_name"] = get_option("payee_name");
+$data["upi_link"] = get_option("upi_link");
+$data["amount"] = get_option("amount");
+?>
+    <script type="text/javascript">
+        jQuery('input[name=payee_name]').val('<?php echo $data["payee_name"]; ?>');
+        jQuery('input[name=upi_link]').val('<?php echo $data["upi_link"]; ?>');
+        jQuery('input[name=amount]').val('<?php echo $data["amount"]; ?>');
+    </script>
+<script type="text/javascript">
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+    $(".ui.dropdown").dropdown();
+</script>
+<?php
+/* Powered By Haysky Code Generator: KEY
+[["text","payee_name"],["text","upi_link"],["text","amount"],["submit","Settings"]]
+*/
+?>
---
+++ donation_qr.php
@@ -0,0 +1,15 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+
+<div id="qrcode"></div>
+<script type="text/javascript">
+
+pa = '<?php echo get_option('upi_link'); ?>';
+pn = '<?php echo get_option('payee_name'); ?>';
+am = '<?php echo get_option('amount'); ?>';
+
+url = 'upi://pay?pa=' + pa + '&pn=' + pn + '&cu=INR&am=' + am
+
+new QRCode(document.getElementById("qrcode"), url);
+
+</script>
